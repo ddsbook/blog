d23dcb61226455dfe5dc934a07b05fc4c17b40ce
boB Rudis <bob@rudis.net>
2014-01-01 13:08:22 -0500

    testing adding of bob's vis

--- 201401-dds-github.md
+++ 201401-dds-github.md
@@ -7,6 +7,7 @@ Slug: 201401-dds-github
 Author: Bob Rudis (@hrbrmstr) and Jay Jacobs (@jayjacobs)
 
 <link rel="stylesheet" type="text/css" href="/blog/extra/201401-dds-github1.css">
+<link rel="stylesheet" type="text/css" href="/blog/extra/201401-dds-github2.css">
 
 We have wrapped up the book and we are launching this website, blog and podcast to continue the discussion. 
 But before we look forward, we thought it would be a good idea to look back at what we put into writing the book.
@@ -15,8 +16,19 @@ thing we want to look at is the days and times of our checkins.  Take a look...
 
 <div id="chart"></div>
 
-Seeing if bob can add to it
+Seeing if bob can add to it:
+
+<center>
+<div style="width:630px;padding:0;margin:0">
+	<div style="width:630px;padding:0;margin:0" id="commits" class="commits"></div>
+		<ul id="ch">
+		</ul>
+		<div style="clear:both"></div>
+		<div style="margin:auto; font-weight:300; font-family:'Lato','Helvetica-Neue','Helvetica','Arial','sans-serif'" id="info"></div>
+</div>
+</center>	
 
 <script type="text/javascript" src="/blog/extra/201401-dds-github1.js"></script>
+<script type="text/javascript" src="/blog/extra/201401-dds-github2.js"></script>
 
 
---
+++ 201401-dds-github2.js
@@ -0,0 +1,136 @@
+						
+function bookGitCommits() { 
+
+	var col ;
+
+	var format = d3.time.format("%Y-%m-%d");
+	var margin = {top: 10, right: 15, bottom: 40, left: 15}
+	var width = 630 - margin.left - margin.right ;
+  var height = 400 - margin.top - margin.bottom ;
+
+	var commits = d3.select("#commits").append("svg")
+					    .attr("width", width + margin.left + margin.right)
+					    .attr("height", height + margin.top + margin.bottom)
+					    .append("g")
+					    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+	d3.csv("commits.csv", function(d) {
+	  return {
+	    Day: format.parse(d.Day),
+	    Chapter: d.Chapter,
+	    CommitCount: +d.CommitCount
+	  };
+	}, function(error, rows) {
+
+    // get extents for ranges
+		
+    var dayExtents = d3.extent(rows, function(d){return d.Day});
+    var commitCountExtents = d3.extent(rows, function(d){return d.CommitCount});
+
+		var Y = d3.scale.linear()
+          .domain(commitCountExtents)
+          .range([height, 0]);
+		var X = d3.time.scale()
+          .domain(dayExtents)
+          .range([margin.left, width]);
+					
+		var month_format = d3.time.format("%b");							
+
+    var xAxis = d3.svg.axis().scale(X).ticks(6).tickFormat(month_format);
+    var yAxis = d3.svg.axis().scale(Y).ticks(6).orient("left");
+
+		 commits.append("g")
+		      .attr("class", "y axis")
+		      .attr("transform", "translate(5,0)")
+		      .call(yAxis);
+
+		 commits.append("g")
+		      .attr("class", "x axis")
+		      .attr("transform", "translate(0," + (height+margin.top) + ")")
+		      .call(xAxis);
+												
+		
+		 var col = d3.scale.category20().domain(d3.set(rows.map(function(d) { return(d.Chapter);})).values())
+
+	 	 var g = commits.append("g")
+		            .attr("transform", "translate(0,0)");
+
+	   g.selectAll(".dot")
+	       .data(rows)
+	       .enter()
+	 			.append("circle")
+	 			.attr("class", function(d) { return("dot d-" + d.Chapter.replace("\.","")) })
+	       .attr("cx", function(d) { return(X(d.Day)); })
+	       .attr("cy", function(d) { return(Y(0)); })
+	       .attr("r", function(d) { return(5); })
+	       .style("fill", function(d) { return(col(d.Chapter)); })
+	       .style("stroke", "white")
+	       .style("stroke-width", "1.5px");
+
+		 // gratituitious animation
+
+	   g.selectAll(".dot")
+       .data(rows)
+	 	   .transition()
+			 .ease("bounce")
+	 	   .duration(1000)
+       .attr("cy", function(d) { return(Y(d.CommitCount)); });
+		 
+		 var chapters = d3.set(rows.map(function(d) { return(d.Chapter) })).values().sort() ;
+		 
+		 d3.select("#ch")
+		   .selectAll("li")
+			 .data(chapters)
+			 .enter()
+			 .append("li")
+			 .style("border","1px solid white")
+			 .on("mouseover",function(d) {
+				 
+				 $(this).css("border","1px solid black");
+				 
+				 $.each(chapters, function(i, e) { 
+					 
+					 if (e != d) {
+						 d3.selectAll(".d-" + e.replace("\.",""))
+							 .transition()
+							 .duration(500)
+							 .attr("r", function(f) { 
+								 return(2) ;
+							 });
+					 }
+					 
+					 d3.selectAll(".d-" + d.replace("\.",""))
+						 .transition()
+						 .duration(500)
+						 .attr("r",10)
+						 .each("end", function(d) { d3.select(this.parentNode.appendChild(this)).transition().duration(150)	; })
+				 })
+									 
+				 var chDays = 0, chCommits = 0 ;
+				 $.each(rows, function(i, r) {
+					 if (r.Chapter == d) {
+						 chDays++ ;
+						 chCommits += r.CommitCount ;
+					 }
+				 });
+				 
+				 $("#info").css("color", col(d));
+				 $("#info").text(d + " had " + chCommits + " commits across " + chDays + " days")  
+				 					 
+			 })
+			 .on("mouseout", function(d) {
+				 $("#info").text("")  
+				 $(this).css("border","1px solid white");
+				 d3.selectAll(".dot")
+					 .transition()
+					 .duration(500)
+		       .style("fill", function(d) { return(col(d.Chapter)); })
+		       .attr("r", function(d) { return(5); })
+			 })
+			 .text(function(d) { return(d) })
+			 .style("background-color", function(d) { return(col(d)) })
+			
+	});					
+};
+
+bookGitCommits();
