ee505a8037e5f06e8e74647093b58808ba3e4796
boB Rudis <bob@rudis.net>
2014-01-20 16:53:32 -0500

    updated shiny code

--- shiny/solvemediocris/server.R
+++ shiny/solvemediocris/server.R
@@ -3,55 +3,75 @@ library(mc2d)
 library(ggplot2)
 library(scales)
  
-N <- 50000
- 
 shinyServer(function(input,output){
-
-		simulate <- reactive( {
+	
+	values <- reactiveValues() 
+	
+	values$N <- 50000
+	values$run <- "no"
+	
+	observe({
+		
+		if (input$runmodel != 0) {
 			
-			TEFestimate <- data.frame(L = input$tefl,  ML = input$tefml,  H = input$tefh, CONF = input$tefconf)
-			TSestimate <- data.frame(L = input$tcapl,  ML = input$tcapml,  H = input$tcaph, CONF = input$tcapconf)
-			RSestimate <- data.frame(L = input$csl,  ML = input$csml,  H = input$csh, CONF = input$csconf)
-			LMestimate <- data.frame(L = input$lml, ML = input$lmml, H = input$lmh, CONF = 1)
-
-			LMsample <- function(x){
-			  return(sum(rpert(x, LMestimate$L, LMestimate$ML, LMestimate$H, shape = LMestimate$CONF) ))
-			}
-
-			TEFsamples <- rpert(N, TEFestimate$L, TEFestimate$ML, TEFestimate$H, shape = TEFestimate$CONF)
-			TSsamples <- rpert(N, TSestimate$L, TSestimate$ML, TSestimate$H, shape = TSestimate$CONF)
-			RSsamples <- rpert(N, RSestimate$L, RSestimate$ML, RSestimate$H, shape = RSestimate$CONF)
-
-			VULNsamples <- TSsamples > RSsamples
-			LEF <- TEFsamples[VULNsamples]
-
-			return(sapply(LEF, LMsample))
+			isolate({
 				
-		})
-  
-    output$plot <- renderPlot({
-			
-			ALEsamples <- simulate()
-			
+				values$N <- input$N
+
+				TEFestimate <- data.frame(L = input$tefl,  ML = input$tefml,  H = input$tefh, CONF = input$tefconf)
+				TSestimate <- data.frame(L = input$tcapl,  ML = input$tcapml,  H = input$tcaph, CONF = input$tcapconf)
+				RSestimate <- data.frame(L = input$csl,  ML = input$csml,  H = input$csh, CONF = input$csconf)
+				LMestimate <- data.frame(L = input$lml, ML = input$lmml, H = input$lmh, CONF = 1)
+		
+				LMsample <- function(x){
+				  return(sum(rpert(x, LMestimate$L, LMestimate$ML, LMestimate$H, shape = LMestimate$CONF) ))
+				}
+		
+				TEFsamples <- rpert(values$N, TEFestimate$L, TEFestimate$ML, TEFestimate$H, shape = TEFestimate$CONF)
+				TSsamples <- rpert(values$N, TSestimate$L, TSestimate$ML, TSestimate$H, shape = TSestimate$CONF)
+				RSsamples <- rpert(values$N, RSestimate$L, RSestimate$ML, RSestimate$H, shape = RSestimate$CONF)
+		
+				VULNsamples <- TSsamples > RSsamples
+				LEF <- TEFsamples[VULNsamples]
+		
+				values$ALEsamples <- sapply(LEF, LMsample)
+				values$VAR <- quantile(values$ALEsamples, probs=(0.95))
+				
+			})
+
+		}
+		
+	})
+	
+	output$detail <- renderPrint({
+		if (input$runmodel != 0) {
+			print(summary(values$ALEsamples));
+		}
+	})
+	
+	output$detail2 <- renderPrint({
+		if (input$runmodel != 0) {
+			print(paste0("Losses at 95th percentile are $", format(values$VAR, nsmall = 2, big.mark = ",")));
+		}
+	})
+	
+		  
+	output$plot <- renderPlot({
+
+		if (input$runmodel != 0) {
+		
+			ALEsamples <- values$ALEsamples 
+						
 			gg <- ggplot(data.frame(ALEsamples), aes(x = ALEsamples))
 			gg <- gg + geom_histogram(binwidth = diff(range(ALEsamples)/50), aes(y = ..density..), color = "black", fill = "white")
 			gg <- gg + geom_density(fill = "steelblue", alpha = 1/3)
 			gg <- gg + scale_x_continuous(labels = comma)
 			gg <- gg + theme_bw()
-			
+		
 			print(gg)
 
-    })
-     
-    output$detail <- renderPrint({			
-			ALEsamples <- simulate()
-			print(summary(ALEsamples));
-    })
-     
-    output$detail2 <- renderPrint({
-			ALEsamples <- simulate()
-			VAR <- quantile(ALEsamples, probs=(0.95))
-			print(paste0("Losses at 95th percentile are $", format(VAR, nsmall = 2, big.mark = ",")));
-    })
+		}	
+
+	})
      
 })
--- shiny/solvemediocris/ui.R
+++ shiny/solvemediocris/ui.R
@@ -6,7 +6,9 @@ shinyUI(pageWithSidebar(
 			
 			tags$head(
         tags$style(type="text/css", "input { font-size:10px; width:40px; display:inline-block; }"),
-        tags$style(type="text/css", "#lml, #lmml, #lmh, #lmconf { font-size:10px; width:100px; display:inline-block; }"),
+        tags$style(type="text/css", "#lml, #lmml, #lmh, #lmconf { font-size:11px; width:100px; display:inline-block; }"),
+        tags$style(type="text/css", "#N { font-size:14px; width:200px; display:inline-block; }"),
+        tags$style(type="text/css", "label[for=N] { font-size:14px; display:inline-block; }"),
         tags$style(type="text/css", "label { font-size:10px; display:inline-block; }")
 			),
 			
@@ -33,8 +35,12 @@ shinyUI(pageWithSidebar(
 	    numericInput("lmml", "ML:", 500, min = 0), br(),
 	    numericInput("lmh", "Max:", 10000, min = 0),
 	    numericInput("lmconf", "Conf:", 1, min = 1, max = 5), br(),
+	    
+			numericInput("N", "# Iterations:", 50000, min = 1000, step=1000), br(),
 			
-			div(HTML("<small>(App brought to you by <a href='http://datadrivensecurity.info'>Data Driven Security</a>)</small>"))
+			actionButton("runmodel", "Run Model"),
+			
+			div(HTML("<br/><small>(App brought to you by <a href='http://datadrivensecurity.info'>Data Driven Security</a>)</small>"))
 			
     ),
 		
